52a0fe64515ce554d1bcc6ad404e0e9d809da38d
Eric Kim <hyeokjungkim@gmail.com>
2018-08-15 12:19:37 -0400

    Added solution

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -2,6 +2,44 @@ class DucksController < ApplicationController
 
   def index
     @ducks = Duck.all
+    render :index
+  end
+
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    # byebug
+    @duck = Duck.new(duck_params)
+    if @duck.save
+      redirect_to duck_path(@duck)
+    else
+      flash[:errors] = @duck.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    if @duck.update(duck_params)
+      redirect_to duck_path(@duck)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
   end
 
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,5 @@
 class Duck < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  belongs_to :student
 end
